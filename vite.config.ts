f9404bfbb27efedd738e93dd2f6246814ee8d6a0
taskylizard <75871323+taskylizard@users.noreply.github.com>
2026-02-06 15:57:50 +0100

    fix: set pages base

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,11 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 import UnoCSS from "unocss/vite";
 
+const pagesBase = process.env.GITHUB_PAGES ? "/toys/" : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: pagesBase,
   plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS()],
   resolve: {
     alias: {
